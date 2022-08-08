6005016cdafda50aafe1839f44c8c25fb8d6b50c
Yuliya Chykiliova <mashkaro77@gmail.com>
2022-08-08 11:57:51 +0200

    feat: added stack for deployment

--- lib/demo-eks-stack.ts
+++ lib/demo-eks-stack.ts
@@ -1,16 +1,32 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as eks from 'aws-cdk-lib/aws-eks';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class DemoEksStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const clusterAdmin = new iam.Role(this, 'AdminRole', {
+      assumedBy: new iam.AccountRootPrincipal()
+      });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DemoEksQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cluster = new eks.Cluster(this, 'sample-eks', {
+      mastersRole: clusterAdmin,
+      version: eks.KubernetesVersion.V1_21,
+      clusterName: "demo-eks",
+      defaultCapacity: 0
+    });
+
+    cluster.addNodegroupCapacity('extra-ng-spot', {
+      instanceTypes: [
+        new ec2.InstanceType('c5.large'),
+        new ec2.InstanceType('c5a.large'),
+        new ec2.InstanceType('c5d.large'),
+      ],
+      minSize: 3,
+      capacityType: eks.CapacityType.SPOT,
+    });
   }
 }
